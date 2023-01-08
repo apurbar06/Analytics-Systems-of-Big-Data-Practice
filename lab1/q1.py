9adbcec060224ea6bc735eb7de5e05941114383a
apurbar06 <apurbar06@gmail.com>
2023-01-08 23:23:00 +0530

    done q1

--- lab1/q1.py
+++ lab1/q1.py
@@ -1,28 +1,21 @@
-import numpy as np
-import os #to change working directory
-import pandas as pd #to work with dataframes/
+import pandas as pd 
+
+
+
+DataCSV=pd.read_csv('colors.csv',na_values=["NA"]) #blank cells read as NAN
+FreqTable=(pd.DataFrame({'Frequency' : DataCSV.groupby( [ "Colour"] ).size()}).reset_index())
+
 
 tallies=[]
-# masking
-tally=""
-def TallyMarks(count):
-    global tally
+for count in FreqTable["Frequency"]:
+    tally=""
     for i in range(1,count+1):
         if(i%5==0 ):
-            tally=tally+'/'
+            tally=tally+'\ '
         else:
-            tally=tally+'l'
+            tally=tally+'|'
     tallies.append((tally))
-    tally=""
-        
     
-#Opening CSv File
-DataCSV=pd.read_csv('ColourData.csv',na_values=["NA"]) #blank cells read as NAN
-#convert junk values to NAN by passing them as list to paramter na_values
-FreqTable=(pd.DataFrame({'Frequency' : DataCSV.groupby( [ "Colour"] ).size()}).reset_index())
-#print(FreqTable)
-for i in FreqTable["Frequency"]:
-    TallyMarks(i)
+
 FreqTable["Tallies"]=tallies    
-#print(tallies)
 print(FreqTable)
